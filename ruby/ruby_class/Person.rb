4c174637f989c2e687a3c87e236013fa7153b9ab
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2015-12-11 15:43:47 -0800

    Add a Person class

--- ruby/ruby_class/Person.rb
+++ ruby/ruby_class/Person.rb
@@ -15,3 +15,18 @@ class Person
 
   end #end of person class
 
+git filter-branch --env-filter '
+OLD_EMAIL="calivn.settachatgul@gmail.com"
+CORRECT_NAME="Calvin Settachatgul"
+CORRECT_EMAIL="calvin.settachatgul@gmail.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
