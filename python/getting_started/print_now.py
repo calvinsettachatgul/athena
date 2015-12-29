643a74ba4172976652732056d1e0257daf51ddfb
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2015-12-29 15:15:26 -0800

    Add example code for importing datetime module

---
+++ python/getting_started/print_now.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+print "We imported datetime module functions within a variable called datetime"
+
+now = datetime.now()
+print "We created a python variable now representing the current date with datetime.now()"
+
+print "Let's print the current year, month and day"
+print now.year
+print now.month
+print now.day
