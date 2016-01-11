d028f52cadfff6b2975c284002efb566f3dd8e81
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2016-01-11 10:45:07 -0800

    Add another route mean/app.js

--- mean/app.js
+++ mean/app.js
@@ -3,9 +3,12 @@ var app = express();
 
 app.get('/', function(req, res){
   res.send('Hello World!');
-})
+});
+
+app.get('/about', function(req, res){
+  res.send('about');
+});
 
 app.listen(3000, function(){
   console.log('Example app listening on port 3000!');
-})
-
+});
