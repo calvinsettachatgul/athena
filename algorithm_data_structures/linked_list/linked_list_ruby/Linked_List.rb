63a6c36c82dd279e87063218802cfd4e1e4ff053
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2016-07-03 17:47:34 -0700

    some minor edits

--- algorithm_data_structures/linked_list/linked_list_ruby/Linked_List.rb
+++ algorithm_data_structures/linked_list/linked_list_ruby/Linked_List.rb
@@ -5,11 +5,9 @@ class LinkedList
 		head_node = Node.new(value)
 		self.head = head_node
 	end
-
 	def print_list()
 		current_node = @head
 		while (current_node != nil) do
-			puts "hello"
 			puts current_node.value
 			current_node = current_node.next
 		end
--- algorithm_data_structures/linked_list/linked_list_ruby/Linked_List_test.rb
+++ algorithm_data_structures/linked_list/linked_list_ruby/Linked_List_test.rb
@@ -4,4 +4,4 @@ my_linked_list = LinkedList.new(1)
 
 my_linked_list.print_list
 
-puts "ran the test"
+puts "ran the Linked_List_test"
