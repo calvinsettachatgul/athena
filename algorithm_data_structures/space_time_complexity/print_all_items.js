723d71ba8c0a634a7938bd4505d800295a7952e8
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2016-07-18 20:42:40 -0700

    going over space and time

---
+++ algorithm_data_structures/space_time_complexity/print_all_items.js
@@ -0,0 +1,11 @@
+function printAllItems(array){
+	for(var i = 0; i < array.length; i++){
+		console.log(array[i]);
+	}
+}
+
+
+arr1 = [1,2,3,4];
+arr2 = [5,4,3,2,1];
+printAllItems(arr1);
+printAllItems(arr2);
---
+++ algorithm_data_structures/space_time_complexity/print_all_items.rb
@@ -0,0 +1,12 @@
+def print_all_items (arr)
+	for i in 0...arr.size do
+		p arr[i]
+	end
+end
+
+
+arr = [ 1,2,3,4 ]
+arr2 = [ 5,4,3,2,1 ]
+print_all_items(arr)
+print_all_items(arr2)
+
