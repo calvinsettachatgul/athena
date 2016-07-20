572bc74c891700a89b612e74a9aa18bd934b541f
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2016-07-19 18:08:11 -0700

    finish most frequent

--- algorithm_data_structures/space_time_complexity/most_frequent_occurrence.rb
+++ algorithm_data_structures/space_time_complexity/most_frequent_occurrence.rb
@@ -1,6 +1,30 @@
 def most_freq_occur( string )
 	#iterate through the string and generate a hash
+	letter_frequency = {}
+	for char in 0...string.size do
+		p string[char]
+		if(letter_frequency[string[char]] == nil)
+			letter_frequency[string[char]] = 1
+		else
+			letter_frequency[string[char]] += 1
+		end
+	end
 
 	#iterateh through the hash and find the max
+	max_letter = []
+	letter_frequency.each do | letter, freq | 
+		if(max_letter.size == 0) 
+			max_letter = [letter, freq]
+		elsif(max_letter[1] < freq)
+			max_letter = [letter, freq]
+		end
+	end
+	p letter_frequency
+	max_letter
 
 end
+
+
+message = "Hello there calvin"
+
+p most_freq_occur( message )
