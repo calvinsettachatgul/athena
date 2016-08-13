78bad052fe8a0c4c3312fa1deb32063a73c02e01
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2016-08-12 21:52:26 -0700

    add client server

---
+++ node/client_server/readme.md
@@ -0,0 +1,11 @@
+#### node client and server 
+
+######  client and server communicate through a protocol
+	##### creates a socket
+	##### ftp or http
+	
+
+##### there is a unique ip address
+
+##### there is also a port 
+
--- node/fileio/fileio.js
+++ node/fileio/fileio.js
@@ -1,10 +1,14 @@
 var fs = require('fs');
 
+
+// read the contents of a file
 var readMe = fs.readFileSync('readme.txt', 'utf8');
 
-//console.log(readMe);
+// console.log(readMe);
 
+// write to a file
 fs.writeFileSync('writeMe.txt', readMe);
 
-
+// deletes a file
+fs.unlink( 'writeMe.txt');
 
