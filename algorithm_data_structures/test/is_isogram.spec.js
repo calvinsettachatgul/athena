a82dcbe0321367a9fc6cda0b835a3fe387f4395b
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2018-11-23 20:27:09 -0800

    feature-5: is_isogram working algorithm

--- algorithm_data_structures/test/is_isogram.spec.js
+++ algorithm_data_structures/test/is_isogram.spec.js
@@ -6,9 +6,21 @@ let isIsogram = require('../is_isogram.js');
 
 describe('#isIsogram', function () {
   context('letters given', function () {
-    it('should work when only one letter given', function () {
-      assert.equal(isIsogram('a'), true);
+    context('when the string is an isogram', function() {
+      it('should work when only one letter given', function () {
+        assert.equal(isIsogram('a'), true);
+      });
     });
-  });
+    context('when the string is not an isogram', function (){
+      it('should return false when letters repeat', function () {
+        assert.equal(isIsogram('aa'), false);
+      });
+      context('when repeating letters are non consecutive', function () {
+        it('should return false when letters repeat', function () {
+          assert.equal(isIsogram('aba'), false);
+        });
+      })
+    })
+  })
 });
 
