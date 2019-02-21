adf39c147432c97716121147cd34691f25049af1
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2019-02-21 09:03:49 -0800

    feature-10-implement-promise-javascript

--- javascript/promise_javascript/promise_example.js
+++ javascript/promise_javascript/promise_example.js
@@ -3,11 +3,18 @@ var promise1 = new Promise(function(resolve, reject) {
     resolve('foo');
   }, 800);
 });
+console.log({ promise1 });
 
 promise1.then(function(value) {
   console.log(value);
   // expected output: "foo"
-});
+})
+.then( () => {
+  console.log({ promise1 });
+  console.log('done');
+})
+
+console.log({ promise1 });
 
 console.log(promise1);
 // expected output: [object Promise]
